ad893945efb1fda9dc6cf0a1bd474a4bac0710fe
cantwellnc <cantwell.nc@gmail.com>
2021-01-08 14:58:01 -0500

    intial commit for heart ratemonitor. Unfortunately, FitBit doesn't directly expose data over a BLE connection. You can only get this data by interfacing with their API, which kind of defeats the purpose of using the Web Bluetooth API. Intention was to display animations of beating heart; can't do this (because no data), so put a gif there instead. End goal was: interface with Spotify API to play random song with bpm = heartrate.

---
+++ heart-rate-monitor/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="heart" src="./assets/heart2.gif">
+  <heart-rate-display></heart-rate-display>
+</template>
+
+<script>
+import HeartRateDisplay from './components/HeartRateDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeartRateDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+  height: 100%;
+}
+img {
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px #111111;
+  margin-bottom: 20px;
+}
+</style>
---
+++ heart-rate-monitor/src/components/HeartRateDisplay.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="hello">
+    <div v-if = "isDeviceConnected">
+      <h1>connected!</h1> 
+      <h3>{{ heartRate }}</h3>
+    </div>
+    <button v-if = "!isDeviceConnected" @click = "requestBluetoothConnection">Search for Bluetooth Devices</button>
+    <button v-else @click = "revokeBluetoothConnection">Disconnect</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeartRateDisplay',
+
+  data() {
+    return { heartRate: 0, isDeviceConnected: false, device: {}, server: {}}; 
+  }, 
+
+  methods: {
+
+    async requestBluetoothConnection() {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{
+          name: "Versa Lite",
+        }], 
+        optionalServices: ["heart_rate"],
+        }); 
+      this.device = device; 
+      const server = await device.gatt.connect();
+      if(server){
+        this.isDeviceConnected = true;
+        this.server = server;
+        this.getHeartRate(server); 
+      } 
+    }, 
+
+    async getHeartRate(server){
+      // Sooooo unfortunately fitbit doesn't allow data to be transmitted over
+      // direct BLE connection, so we can't access this service w/o going through
+      // their API which kinda defeats the purpose of this whole thing
+      try{
+        const hr = await server.getPrimaryService("heart_rate");
+        const hrMeasurement = await hr.getCharateristic("heart_rate_measurement");
+        this.heartRate = hrMeasurement.readValue().getUint8(0);
+      }
+      catch(error){
+        console.warn(error);
+      }
+      
+    }, 
+
+    revokeBluetoothConnection(){
+      this.device.gatt.disconnect();
+      this.isDeviceConnected = false;
+    }, 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+
+button {
+  padding: 10px 10px;
+  background-color: #385e3c;
+  color: #fefff1;
+  border-radius: 10px;
+  border: none;
+  box-shadow: 2px 2px 5px #111111;
+  transition: background-color 500ms; 
+}
+button:hover {
+  background-color: #486a47;
+}
+</style>
